73769bed444c8eeba0b1558cf184500e218e58c9
Vince <jpinard@bluewin.ch>
2025-02-19 23:13:13 +0100

    init

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
     build: {
         target: "modules",
-        outDir: "../build",
+        outDir: "./dist",
         emptyOutDir: true,
     },
     plugins: [react()]
